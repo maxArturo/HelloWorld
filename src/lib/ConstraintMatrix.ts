0452482f23f67516aa4ab0b2dd8326c276091b54
Max Alcala <maxarturo@gmail.com>
2018-11-23 10:54:17 -0600

    Working implementation

--- src/lib/ConstraintMatrix.ts
+++ src/lib/ConstraintMatrix.ts
@@ -1,7 +1,5 @@
 import { BoardInterface } from './Board';
 import { ConstraintColumnInterface } from './ConstraintColumn';
-import ConstraintColumn from './ConstraintColumn';
-import ConstraintNode, { ConstraintRowInterface } from './ConstraintNode';
 
 /**
  * @class ConstraintMatrix: Converts a hitori board into a constraint matrix data structure
@@ -23,7 +21,7 @@ export default class ConstraintMatrix {
     this.board = inputBoard;
   }
 
-  public generateMatrix(): void {
+  public generateMatrix(): number[][] {
     /*
      * We need to represent three types of constraints:
      * Each possible number in each row (n numbers * n rows)
@@ -33,136 +31,26 @@ export default class ConstraintMatrix {
      * For each constraint type, we'll represent it as a series of columns in our constraint matrix.
      * We'll have a total of (n ^ 2 * 3) columns.
      */
-    this.solutionColumns = Array(this.board.size ** 2 * 3)
-      .fill(0)
-      .map((_, i) => new ConstraintColumn(i))
-      .map((el, i, arr) => {
-        // create links to left, right col nodes
-        const nextEl = arr[(i + 1) % arr.length];
-        el.right = nextEl;
-        nextEl.left = el;
-        return el;
+
+    // our first-order elements in the matrix will be the row options themselves.
+    const matrix = Array(this.board.size ** 3)
+      .fill([])
+      .map((_, i) => {
+        // add cell constraints
+        const cellConstraints = Array(this.board.size ** 2).fill(0);
+        const rowConstraints = Array(this.board.size ** 2).fill(0);
+        const colConstraints = Array(this.board.size ** 2).fill(0);
+
+        cellConstraints[Math.trunc(i / this.board.size)] = 1;
+        rowConstraints[
+          (i % this.board.size) +
+            Math.trunc(i / this.board.size ** 2) * this.board.size
+        ] = 1;
+        colConstraints[i % this.board.size ** 2] = 1;
+
+        return cellConstraints.concat(rowConstraints).concat(colConstraints);
       });
 
-    // iterate through all board node/number combinations
-    for (let num = 0; num < this.board.size; num++) {
-      for (let x = 0; x < this.board.size; x++) {
-        for (let y = 0; y < this.board.size; y++) {
-          const currNode = this.board.nodeAt([x, y]);
-          if (currNode) {
-            // add for column
-            const constraintCol = this.solutionColumns[
-              num * this.board.size + x
-            ];
-            const constraintColNode = new ConstraintNode(currNode, num + 1);
-            constraintColNode.label = `column constraint: no:${num +
-              1} at coordinates(${x}, ${y}) column_id: ${constraintCol.id}`;
-            constraintColNode.column = constraintCol;
-            this.appendToColumn(constraintCol, constraintColNode);
-
-            // add for row
-            const constraintRow = this.solutionColumns[
-              this.board.size ** 2 + num * this.board.size + y
-            ];
-            const constraintRowNode = new ConstraintNode(currNode, num + 1);
-            constraintRowNode.label = `row constraint: no:${num +
-              1} at coordinates(${x}, ${y}) column_id: ${constraintRow.id}`;
-            constraintRowNode.column = constraintRow;
-            this.appendToColumn(constraintRow, constraintRowNode);
-
-            constraintColNode.right = constraintRowNode;
-            constraintRowNode.left = constraintColNode;
-
-            // add for cell. If this cell is marked, we don't care about its constraint (optimization)
-            if (!currNode.marked) {
-              const constraintCell = this.solutionColumns[
-                2 * this.board.size ** 2 + currNode.id
-              ];
-              const constraintCellNode = new ConstraintNode(currNode, num + 1);
-              constraintCellNode.label = `cell constraint: no:${num +
-                1} at coords(${x}, ${y}) column_id:${constraintCell.id}`;
-              constraintCellNode.column = constraintCell;
-              this.appendToColumn(constraintCell, constraintCellNode);
-
-              constraintRowNode.right = constraintCellNode;
-              constraintCellNode.left = constraintRowNode;
-              constraintCellNode.right = constraintColNode;
-              constraintColNode.left = constraintCellNode;
-            } else {
-              constraintColNode.left = constraintRowNode;
-              constraintRowNode.right = constraintColNode;
-            }
-          }
-        }
-      }
-    }
-
-    this.addMainNode();
-    this.filterUnusedCols();
-    // this.printConstraintMap();
-  }
-
-  private filterUnusedCols(): void {
-    let currCol = this.main.right;
-    while (currCol !== this.main) {
-      if (!currCol.count) {
-        currCol.left.right = currCol.right;
-        currCol.right.left = currCol.left;
-      }
-      currCol = currCol.right;
-    }
-  }
-
-  public printConstraintMap(): void {
-    let currCol: ConstraintColumnInterface = this.main
-      .right as ConstraintColumnInterface;
-
-    while (!currCol.main) {
-      const nodeContents = [];
-
-      let currNode = currCol.down as ConstraintRowInterface;
-      while (currNode.node) {
-        nodeContents.push(
-          `solution ${currNode.solutionNumber} at [${
-            currNode.node.coordinates
-          }]`,
-        );
-        currNode = currNode.down as ConstraintRowInterface;
-      }
-
-      console.log(`constraint col ${currCol.id}: ${nodeContents.join(', ')}`);
-      currCol = currCol.right as ConstraintColumnInterface;
-    }
-  }
-
-  private addMainNode(): void {
-    // add a main node, wire it up with the rest of the columns
-    this.main = new ConstraintColumn(-1, true);
-
-    this.main.right = this.solutionColumns[0];
-    this.main.right.left = this.main;
-
-    this.main.left = this.solutionColumns.slice(-1)[0];
-    this.main.left.right = this.main;
-
-    this.solutionColumns.unshift(this.main);
-  }
-
-  private appendToColumn(
-    colHead: ConstraintColumnInterface,
-    node: ConstraintRowInterface,
-  ): void {
-    if (colHead.count) {
-      const last = colHead.up;
-      colHead.up = node;
-      node.down = colHead;
-      node.up = last;
-      last.down = colHead.up;
-    } else {
-      colHead.up = colHead.down = node;
-      node.up = node.down = colHead;
-    }
-
-    colHead.count++;
+    return matrix;
   }
 }
--- src/lib/ConstraintSolver.ts
+++ src/lib/ConstraintSolver.ts
@@ -2,8 +2,9 @@ import { BoardInterface } from './Board';
 import ConstraintMatrix, {
   ConstraintMatrixInterface,
 } from './ConstraintMatrix';
-import ConstraintNode, { ConstraintRowInterface } from './ConstraintNode';
-import { ConstraintColumnInterface } from './ConstraintColumn';
+import { sample, shuffle } from 'lodash';
+
+const solver = require('dlxlib');
 
 /**
  * @class ConstraintSolver implements the Algorithm X, relying on a data structure
@@ -15,122 +16,18 @@ export default class ConstraintSolver {
   constructor(board: BoardInterface) {
     this.constraintMatrix = new ConstraintMatrix(board);
 
-    this.constraintMatrix.generateMatrix();
-    this.generateSolution();
-  }
+    const result = this.constraintMatrix.generateMatrix();
+    console.log(result);
+    console.table(result);
+    const solution: number[] = sample(solver.solve(result, null, null, 2048));
+    shuffle(solution).map(el => {
+      const x = Math.trunc(el / board.size ** 2);
+      const y = Math.trunc(el / board.size) % board.size;
 
-  private generateSolution(): void {
-    const solution = this.solveNextConstraint();
-    console.log('printing solver solution:', solution);
-  }
-
-  private solveNextConstraint(
-    currSolution: ConstraintRowInterface[] = [],
-    depth = 0,
-  ): ConstraintRowInterface[] {
-    if (this.constraintMatrix.main.right === this.constraintMatrix.main) {
-      // if no unsolved constraints remain (no columns to the right of header) return
-      return currSolution;
-    }
-    let nextConstraint = this.getLowestCol();
-
-    if (nextConstraint !== null) {
-      this.coverColumn(nextConstraint);
-      let nextRow = nextConstraint.down;
-      while (nextRow !== nextConstraint) {
-        // add row to our solution
-        currSolution.push(nextRow as ConstraintRowInterface);
-
-        // cover each column that is satisfied by row
-        let rightRow = nextRow.right as ConstraintRowInterface;
-        while (rightRow !== nextRow) {
-          this.coverColumn(rightRow.column);
-          rightRow = rightRow.right as ConstraintRowInterface;
-        }
-        // search recursively
-
-        const result = this.solveNextConstraint(currSolution, depth + 1);
-        if (result.length) {
-          return result;
-        }
-        // solution not found; pop latest row and use its column
-        nextRow = currSolution.pop() as ConstraintRowInterface;
-        nextConstraint = (nextRow as ConstraintRowInterface).column;
-
-        // uncover in reverse order
-        let leftRow = nextRow.left as ConstraintRowInterface;
-        while (leftRow !== nextRow) {
-          this.uncoverColumn(leftRow.column);
-          leftRow = leftRow.left as ConstraintRowInterface;
-        }
-        nextRow = nextRow.down;
+      const node = board.nodeAt([x, y]);
+      if (node) {
+        node.solutionNumber = ((el % board.size) + 1).toString();
       }
-      this.uncoverColumn(nextConstraint);
-    }
-
-    return [];
-  }
-
-  /*
-   * @function getLowestCol() gets the matrix column with the lowest number of associated rows.
-   * If there are no columns, it returns null. If there are two or more nodes with the same number of rows,
-   * it returns the column closest to the right of the head node.
-   */
-  private getLowestCol(): ConstraintColumnInterface | null {
-    if (this.constraintMatrix.main.right === this.constraintMatrix.main) {
-      return null;
-    }
-
-    let smallestNumCol = this.constraintMatrix.main.right;
-    let nextCol = smallestNumCol.right;
-
-    while (nextCol !== this.constraintMatrix.main) {
-      if (smallestNumCol.count > nextCol.count) {
-        smallestNumCol = nextCol;
-      }
-      nextCol = nextCol.right;
-    }
-
-    return smallestNumCol;
-  }
-
-  /*
-   * @function coverColumn() implements the reversible "covering" of a column, as described here:
-   * http://garethrees.org/2007/06/10/zendoku-generation/#section-4.3
-   */
-  private coverColumn(column: ConstraintColumnInterface): void {
-    column.right.left = column.left;
-    column.left.right = column.right;
-    let node = column.down;
-    while (node !== column) {
-      let rightNode = node.right;
-      while (node !== rightNode) {
-        rightNode.down.up = rightNode.up;
-        rightNode.up.down = rightNode.down;
-        (rightNode as ConstraintRowInterface).column.count--;
-        rightNode = rightNode.right;
-      }
-      node = node.down;
-    }
-  }
-
-  /*
-   * @function uncoverColumn() implements the reversible "uncovering" of a column, as described here:
-   * http://garethrees.org/2007/06/10/zendoku-generation/#section-4.3
-   */
-  private uncoverColumn(column: ConstraintColumnInterface): void {
-    let node = column.up;
-    while (node !== column) {
-      let leftNode = node.left;
-      while (leftNode !== node) {
-        (leftNode as ConstraintNode).column.count++;
-        leftNode.down.up = leftNode;
-        leftNode.up.down = leftNode;
-        leftNode = leftNode.left;
-      }
-      node = node.up;
-    }
-    column.right.left = column;
-    column.left.right = column;
+    });
   }
 }
