e38e11ef33b3730e7194fb0f62a9a99a981b2675
Max Alcala <maxarturo@gmail.com>
2018-11-25 16:54:17 -0600

    Formatting/style changes

--- src/Components/Block.tsx
+++ src/Components/Block.tsx
@@ -2,26 +2,39 @@ import * as React from 'react';
 import Color from '../lib/Color';
 import './Block.css';
 
-export interface BlockProps {
+export interface BlockPropsInterface {
   color: Color;
   coordinates: number[];
   value: string;
   onClick: () => void;
 }
 
-export default class Block extends React.Component<BlockProps, {}> {
-  constructor(props: BlockProps) {
+export interface BlockStateInterface {
+  selected: boolean;
+}
+
+export default class Block extends React.Component<
+  BlockPropsInterface,
+  BlockStateInterface
+> {
+  constructor(props: BlockPropsInterface) {
     super(props);
+    this.state = { selected: false };
   }
 
   render() {
     return (
       <div
         className="board-block"
-        style={{ backgroundColor: this.props.color }}
-        onClick={this.props.onClick}
+        style={{
+          backgroundColor: this.state.selected ? Color.Black : Color.White,
+        }}
+        onClick={() => {
+          this.setState({ selected: !this.state.selected });
+          this.props.onClick();
+        }}
       >
-        {this.props.value}
+        <div className="block-number">{this.props.value}</div>
       </div>
     );
   }
